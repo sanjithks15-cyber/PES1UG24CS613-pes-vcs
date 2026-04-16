79e06223c34fcb164fc8c232467b3b80ced0ddfc
sanjithks15-cyber <sanjithks15@gmail.com>
2026-04-16 11:43:43 +0530

    handle initial commit with no parent reference

--- commit.c
+++ commit.c
@@ -200,11 +200,12 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     if (tree_from_index(&commit.tree) != 0)
         return -1;
 
-    if (head_read(&commit.parent) == 0)
-        commit.has_parent = 1;
-    else
-        commit.has_parent = 0;
-
+   if (head_read(&commit.parent) == 0) {
+    commit.has_parent = 1;
+} else {
+    memset(&commit.parent, 0, sizeof(ObjectID));
+    commit.has_parent = 0;
+}
     snprintf(commit.message, sizeof(commit.message), "%s", message);
 
 snprintf(commit.author, sizeof(commit.author), "%s", pes_author());
